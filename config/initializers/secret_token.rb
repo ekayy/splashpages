034f1bfeff9642feffcf3234dd1b48d19f268ef2
Eric K Wu <ekaun.wu@gmail.com>
2012-05-16 16:01:50 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Splashpages::Application.config.secret_token = '9e0fa8cbc6d6ed4ca549860f878fcf718a714e030799188e7bde9da60015099fbeea7e0581e4529554ef2237d154f6d02f3e9ad0b78dc0e6b57f481389beca59'
